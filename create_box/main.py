aabcefc6e30ba9781d414b003eb1e4bb72be7a75
Michelle Huynh <michhh.12@gmail.com>
2017-03-25 15:32:55 +0000

    added create_border_box function

--- create_box/main.py
+++ create_box/main.py
@@ -9,7 +9,22 @@ def create_box(height, width, character):
             box += character
         box += '\n'
     return box
+    
+def create_border_box(height, width, character):
+    box = ''
+    n = 0
+    space = ' ' * (width-2)
+    for n in range(height):
+        if n == 0 or n == (height-1):
+            for n in range(width):
+                box += character
+            box += '\n'
+        else:
+            box += (character + space + character)
+            box += '\n'
+    return box
 
 
 if __name__ == '__main__':
     create_box(3, 4, '*')
+    create_border_box(20, 20, '*')
