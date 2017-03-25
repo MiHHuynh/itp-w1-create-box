48bd4b0ce49014e3601f48518cbf442e93ce9247
Michelle Huynh <michhh.12@gmail.com>
2017-03-25 15:33:22 +0000

    test for border box

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
 import unittest
 
-from create_box import create_box
+from create_box import create_box, create_border_box
 
 first_box_expected = """
 ****
@@ -18,6 +18,16 @@ xxxxxxxxxxxxxxxxxxxxxxxx
 xxxxxxxxxxxxxxxxxxxxxxxx
 """.lstrip()
 
+fourth_box_expected = """
+^^^^^^^^^^^^^^^
+^             ^
+^             ^
+^             ^
+^             ^
+^             ^
+^             ^
+^^^^^^^^^^^^^^^
+""".lstrip()
 
 class TestCreateBox(unittest.TestCase):
     def test_box(self):
@@ -25,5 +35,11 @@ class TestCreateBox(unittest.TestCase):
 
     def test_small_box(self):
         self.assertEqual(create_box(1, 1, '@'), second_box_expected)
+    
+    def test_large_box(self):
+        self.assertEqual(create_box(3, 24, 'x'), third_box_expected)
+    
+    def test_border_box(self):
+        self.assertEqual(create_border_box(8, 15, '^'), fourth_box_expected)
 
     # Add your own test using third_box_expected
